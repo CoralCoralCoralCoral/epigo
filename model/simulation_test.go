3b499faca97cb8c2042ef4fc5898c88e9e6c00cc
Umran Hussain <1547890+umran@users.noreply.github.com>
2024-12-18 19:37:47 +0000

    refactored simulation params

--- model/simulation_test.go
+++ model/simulation_test.go
@@ -7,23 +7,73 @@ import (
 )
 
 func TestSimulationInitialization(t *testing.T) {
-	pathogen := Pathogen{
-		IncubationPeriod:           [2]float64{3 * 24 * 60 * 60 * 1000, 8 * 60 * 60 * 1000},
-		RecoveryPeriod:             [2]float64{7 * 24 * 60 * 60 * 1000, 8 * 60 * 60 * 1000},
-		ImmunityPeriod:             [2]float64{330 * 24 * 60 * 60 * 1000, 90 * 24 * 60 * 60 * 1000},
-		PrehospitalizationPeriod:   [2]float64{3 * 24 * 60 * 60 * 1000, 8 * 60 * 60 * 1000},
-		HospitalizationPeriod:      [2]float64{7 * 24 * 60 * 60 * 1000, 3 * 24 * 60 * 60 * 1000},
-		QuantaEmissionRate:         [2]float64{500, 150},
-		HospitalizationProbability: 0.15,
-		DeathProbability:           0.75,
-		AsymptomaticProbability:    0.10,
-	}
-
 	config := Config{
-		Id:        uuid.New(),
+		Id: uuid.New(),
+
+		// Global Params
 		TimeStep:  15 * 60 * 1000,
 		NumAgents: 150000,
-		Pathogen:  pathogen,
+
+		// Agent Params
+		ComplianceProbability:        0.65,
+		SeeksTreatmentProbability:    0.4,
+		MaskFiltrationEfficiencyMean: 0.75,
+		MaskFiltrationEfficiencySd:   0.2,
+		PulmonaryVentilationRateMean: 0.36,
+		PulmonaryVentilationRateSd:   0.01,
+
+		// Pathogen Config
+		IncubationPeriodMean:         3 * 24 * 60 * 60 * 1000,
+		IncubationPeriodSd:           8 * 60 * 60 * 1000,
+		RecoveryPeriodMean:           7 * 24 * 60 * 60 * 1000,
+		RecoveryPeriodSd:             8 * 60 * 60 * 1000,
+		ImmunityPeriodMean:           330 * 24 * 60 * 60 * 1000,
+		ImmunityPeriodSd:             90 * 24 * 60 * 60 * 1000,
+		PrehospitalizationPeriodMean: 3 * 24 * 60 * 60 * 1000,
+		PrehospitalizationPeriodSd:   8 * 60 * 60 * 1000,
+		HospitalizationPeriodMean:    7 * 24 * 60 * 60 * 1000,
+		HospitalizationPeriodSd:      3 * 24 * 60 * 60 * 1000,
+		QuantaEmissionRateMean:       500,
+		QuantaEmissionRateSd:         150,
+		HospitalizationProbability:   0.15,
+		DeathProbability:             0.75,
+		AsymptomaticProbability:      0.10,
+
+		// Household Params
+		HouseholdCapacityMean:      4,
+		HouseholdCapacitySd:        2,
+		HouseholdAirChangeRateMean: 7,
+		HouseholdAirChangeRateSd:   1,
+		HouseholdVolumeMean:        17,
+		HouseholdVolumeSd:          2,
+
+		// Office Params
+		OfficeCapacityMean:      10,
+		OfficeCapacitySd:        2,
+		OfficeAirChangeRateMean: 20,
+		OfficeAirChangeRateSd:   5,
+		OfficeVolumeMean:        60,
+		OfficeVolumeSd:          20,
+
+		// Social Space Params
+		SocialSpaceCapacityMean:      10,
+		SocialSpaceCapacitySd:        2,
+		SocialSpaceAirChangeRateMean: 20,
+		SocialSpaceAirChangeRateSd:   5,
+		SocialSpaceVolumeMean:        60,
+		SocialSpaceVolumeSd:          10,
+
+		// Healthcare Space Params
+		HealthcareSpaceCapacityMean:      173,
+		HealthcareSpaceCapacitySd:        25,
+		HealthcareSpaceAirChangeRateMean: 20,
+		HealthcareSpaceAirChangeRateSd:   5,
+		HealthcareSpaceVolumeMean:        60,
+		HealthcareSpaceVolumeSd:          10,
+		TestCapacityMean:                 300,
+		TestCapacitySd:                   150,
+		TestSensitivity:                  0.99,
+		TestSpecificity:                  0.95,
 	}
 
 	sim := NewSimulation(config, NewDefaultEntityGenerator())
